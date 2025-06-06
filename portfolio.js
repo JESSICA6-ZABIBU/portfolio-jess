8f66af44ac53c42a3c3fa7d99c7d6f2b73bd98c7
JESSICA6-ZABIBU <jesszabibu15@gmail.com>
2025-06-06 21:46:39 +0200

    creation du bouton avec fonctionnalite onclick

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+<script>
+    const menuToggle = document.getElementById('menu-toggle');
+    const menuLinks = document.getElementById('menu-links');
+
+    menuToggle.addEventListener('click', () => {
+        menuLinks.classList.toggle('hidden');
+    });
+</script>
